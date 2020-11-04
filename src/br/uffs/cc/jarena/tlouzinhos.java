bd3e71e9e0d7b5db97f46e8ea32d0437bcc88bde
leonardo machado <mestreleozinhoocara@gmail.com>
2020-11-04 13:45:49 -0300

    pasta certo

---
+++ src/br/uffs/cc/jarena/tlouzinhos.java
@@ -0,0 +1,61 @@
+
+package br.uffs.cc.jarena;
+
+public class tlouzinhos extends Agente
+{
+	public tlouzinhos (Integer x, Integer y, Integer energia) {
+		super(500, 600, energia);
+		setDirecao(geraDirecaoAleatoria());
+	}
+	
+	public void pensa() {
+		
+		if(!podeMoverPara(getDirecao())) {
+			
+			setDirecao(geraDirecaoAleatoria());
+		}
+		
+		
+		
+			
+			
+	}
+	
+	public void recebeuEnergia() {
+		enviaMensagem("GANHEI ENERGIA POLA");
+		int a;
+		a = getDirecao();
+		para();
+			
+		
+			
+		
+	}
+	
+	public void tomouDano(int energiaRestanteInimigo) {
+		setDirecao(geraDirecaoAleatoria());
+		
+		
+	}
+	
+	public void ganhouCombate() {
+		
+	}
+	
+	public void recebeuMensagem(String msg) {
+				int a;
+				a = getDirecao();
+				setDirecao(a);
+				
+
+			}
+
+		
+	
+	
+	public String getEquipe() {
+		
+		return "pepo";
+	}
+}
+
